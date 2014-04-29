19f88ab17537db46f4d242a3a92bdc7346a4bd92
mleroi <code@mathieuleroi.com>
2014-04-29 23:21:40 +0200

    Flush rewrite rules on plugin activation

--- wp-content/plugins/wp-phonecast/wp-phonecast.php
+++ wp-content/plugins/wp-phonecast/wp-phonecast.php
@@ -21,11 +21,18 @@ class WpPhoneCast{
 	
 	public static function hooks(){
 		register_activation_hook( __FILE__, array(__CLASS__,'on_activation') );
+		register_deactivation_hook( __FILE__, array(__CLASS__,'on_deactivation') );
 		add_action('init',array(__CLASS__,'init'));
 		add_action('template_redirect',array(__CLASS__,'template_redirect'),5);
 	}
 	
 	public static function on_activation(){
+		WppcWebServices::add_rewrite_tags_and_rules();
+		WppcConfigFile::rewrite_rules();
+		flush_rewrite_rules();
+	}
+	
+	public static function on_deactivation(){
 		flush_rewrite_rules();
 	}
 	
